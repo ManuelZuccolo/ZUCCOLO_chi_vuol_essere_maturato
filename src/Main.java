7e48fe7748f68fad7be77c4411cd6b5d9d8a2c70
ManuelZuccolo <manuelzuccolo2007@gmail.com>
2025-10-01 08:24:02 +0200

    We good?

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main
     public static void main(String[] args)
     {
         ApiClient api = new  ApiClient();
-        System.out.println(api.fetchQuestions(5, "easy", "multiple"));
+        System.out.println(api.fetchQuestions(5, "easy", "multiple"));//
     }
 }
