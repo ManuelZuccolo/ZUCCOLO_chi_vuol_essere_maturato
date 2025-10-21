49c739c5ca88f1e93e0350e1fedf51b64fb7c0bf
Manuz <168524071+ManuelZuccolo@users.noreply.github.com>
2025-10-21 23:21:36 +0200

    Main Stampa domande

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        ApiClient api = new  ApiClient();
+public class Main {
+    public static void main(String[] args) {
+        ApiClient api = new ApiClient();
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Quante domande vuoi? ");
-        int amount=scan.nextInt();
+        int amount = scan.nextInt();
 
+        APIQuestion[] questions = api.fetchQuestions(amount, "easy", "multiple");
 
-        System.out.println(api.fetchQuestions(amount, "easy", "multiple"));//
+        for (APIQuestion q : questions) {
+            api.printQuestion(q);
+            System.out.println();
+        }
     }
 }
